3ea4c06418d82a47113c8e4e4abd4940505ff837
taydakov <lev.taydakov@gmail.com>
2015-06-04 17:43:11 -0700

    Complete NewsItemTitle

---
+++ js/NewsItem.js
@@ -0,0 +1,18 @@
+'use strict'
+
+var $ = require('jquery');
+var React = require('react');
+
+var NewsItem = React.createClass({
+
+	render: function() {
+		return (
+			<div className="newsItem">
+				<a className="newsItem-titleLink" href={this.props.item.url}>{this.props.item.title}</a>
+			</div>
+		);
+	}
+
+});
+
+module.exports = NewsItem;
