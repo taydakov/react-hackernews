8a8d5c8fa2cb7c230e6d8ee52d75652a3f8cdd59
taydakov <lev.taydakov@gmail.com>
2015-06-04 18:25:19 -0700

    Complete NewsItem subtext

--- js/NewsItem.js
+++ js/NewsItem.js
@@ -3,6 +3,7 @@
 var $ = require('jquery');
 var React = require('react');
 var url = require('url');
+var moment = require('moment');
 
 var NewsItem = React.createClass({
 
@@ -10,13 +11,41 @@ var NewsItem = React.createClass({
 		return url.parse(this.props.item.url).hostname;
 	},
 
-	render: function() {
+	getCommentLink: function () {
+		var commentText = 'discuss';
+		if (this.props.item.kids && this.props.item.kids.length) {
+			commentText = this.props.item.kids.length + ' comments';
+		}
+
 		return (
-			<div className="newsItem">
+			<a href={'https://news.ycombinator.com/item?id=' + this.props.item.id}>{commentText}</a>
+		);
+	},
+
+	getSubtext: function () {
+		return (
+			<div className="newsItem-subtext">
+				{this.props.item.score} points by <a href={'https://news.ycombinator.com/user?id=' + this.props.item.by}>{this.props.item.by}</a> {moment.utc(this.props.item.time * 1000).fromNow()} | {this.getCommentLink()}
+			</div>
+		);
+	},
+
+	getTitle: function () {
+		return (
+			<div className="newsItem-title">
 				<a className="newsItem-titleLink" href={this.props.item.url}>{this.props.item.title}</a>
 				<span className="newsItem-domain">
 					({this.getDomain()})
 				</span>
+			</div>
+		);
+	},
+
+	render: function () {
+		return (
+			<div className="newsItem">
+				{this.getTitle()}
+				{this.getSubtext()}
 			</div>
 		);
 	}
